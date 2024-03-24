c58ebc18b94f3da0a1b071798c3b56650a9d5eb5
skdyekdtls <skdyekdtls@gmail.com>
2024-03-24 22:49:34 +0900

    8주차 애니메이션 데이터

--- AssimpTool/AssimpTool.cpp
+++ AssimpTool/AssimpTool.cpp
@@ -10,6 +10,21 @@ void AssimpTool::Init()
 		converter->ExportMaterialData(L"Kachujin/Kachujin");
 		converter->ExportModelData(L"Kachujin/Kachujin");
 	}
+	{
+		shared_ptr<Converter> converter = make_shared<Converter>();
+		converter->ReadAssetFile(L"Kachujin/Idle.fbx");
+		converter->ExportAnimationData(L"Kachujin/Idle");
+	}
+	{
+		shared_ptr<Converter> converter = make_shared<Converter>();
+		converter->ReadAssetFile(L"Kachujin/Run.fbx");
+		converter->ExportAnimationData(L"Kachujin/Run");
+	}
+	{
+		shared_ptr<Converter> converter = make_shared<Converter>();
+		converter->ReadAssetFile(L"Kachujin/Slash.fbx");
+		converter->ExportAnimationData(L"Kachujin/Slash");
+	}
 }
 
 void AssimpTool::Update()
