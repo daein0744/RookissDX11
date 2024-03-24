154a87239ae6698552678d859ed8d7a5091f7574
skdyekdtls <skdyekdtls@gmail.com>
2024-03-25 08:02:58 +0900

    9주차 인스턴싱과 드로우콜

--- Engine/ModelRenderer.cpp
+++ Engine/ModelRenderer.cpp
@@ -14,30 +14,6 @@ ModelRenderer::~ModelRenderer()
 {
 
 }
-//
-//void ModelRenderer::Update()
-//{
-//	if (_model == nullptr)
-//		return;
-//
-//	auto world = GetTransform()->GetWorldMatrix();
-//	RENDER->PushTransformData(TransformDesc{ world });
-//
-//	const auto& meshes = _model->GetMeshes();
-//	for (auto& mesh : meshes)
-//	{
-//		if (mesh->material)
-//			mesh->material->Update();
-//
-//		uint32 stride = mesh->vertexBuffer->GetStride();
-//		uint32 offset = mesh->vertexBuffer->GetOffset();
-//
-//		DC->IASetVertexBuffers(0, 1, mesh->vertexBuffer->GetComPtr().GetAddressOf(), &stride, &offset);
-//		DC->IASetIndexBuffer(mesh->indexBuffer->GetComPtr().Get(), DXGI_FORMAT_R32_UINT, 0);
-//
-//		_shader->DrawIndexed(0, _pass, mesh->indexBuffer->GetCount(), 0, 0);
-//	}
-//}
 
 void ModelRenderer::Update()
 {
