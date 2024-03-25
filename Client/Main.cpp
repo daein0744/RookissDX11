a43800d8be79feda4177627a8ff06c785eac4306
skdyekdtls <skdyekdtls@gmail.com>
2024-03-26 04:15:55 +0900

    선택 강의 System Value

--- Client/Main.cpp
+++ Client/Main.cpp
@@ -3,6 +3,7 @@
 #include "Engine/Game.h"
 #include "SceneDemo.h"
 #include "RawBufferDemo.h"
+#include "GroupDemo.h"
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd)
 {
 	GameDesc desc;
@@ -13,7 +14,7 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 	desc.width = 800;
 	desc.height = 600;
 	desc.clearColor = Color(0.f, 0.f, 0.f, 0.f);
-	desc.app = make_shared<RawBufferDemo>();
+	desc.app = make_shared<GroupDemo>();
 
 	GAME->Run(desc);
 
