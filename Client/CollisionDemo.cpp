4aa4ef34a380e58832f33e33b2ca8fe6a589eab7
skdyekdtls <skdyekdtls@gmail.com>
2024-03-26 12:46:44 +0900

    선택 강의 Terrain Picking

--- Client/CollisionDemo.cpp
+++ Client/CollisionDemo.cpp
@@ -24,7 +24,7 @@
 #include "Scene.h"
 #include "AABBBoxCollider.h"
 #include "OBBBoxCollider.h"
-
+#include "Terrain.h"
 
 void CollisionDemo::Init()
 {
@@ -37,6 +37,19 @@ void CollisionDemo::Init()
 		camera->AddComponent(make_shared<Camera>());
 		camera->AddComponent(make_shared<CameraScript>());
 		CUR_SCENE->Add(camera);
+	}
+
+	// Light
+	{
+		auto light = make_shared<GameObject>();
+		light->AddComponent(make_shared<Light>());
+		LightDesc lightDesc;
+		lightDesc.ambient = Vec4(0.4f);
+		lightDesc.diffuse = Vec4(1.f);
+		lightDesc.specular = Vec4(0.1f);
+		lightDesc.direction = Vec3(1.f, 0.f, 1.f);
+		light->GetLight()->SetLightDesc(lightDesc);
+		CUR_SCENE->Add(light);
 	}
 
 	// Material
@@ -52,10 +65,73 @@ void CollisionDemo::Init()
 		RESOURCES->Add(L"Veigar", material);
 	}
 
+	// Terrain
+	{
+		auto obj = make_shared<GameObject>();
+		obj->AddComponent(make_shared<Terrain>());
+		obj->GetTerrain()->Create(10, 10, RESOURCES->Get<Material>(L"Veigar"));
+
+		CUR_SCENE->Add(obj);
+	}
+	//{
+	//	auto obj = make_shared<GameObject>();
+	//	obj->GetOrAddTransform()->SetLocalPosition(Vec3(0.f));
+	//	obj->AddComponent(make_shared<MeshRenderer>());
+	//	{
+	//		auto mesh = make_shared<Mesh>();
+	//		mesh->CreateGrid(10, 10);
+	//		obj->GetMeshRenderer()->SetMesh(mesh);
+	//		obj->GetMeshRenderer()->SetPass(0);
+	//	}
+	//	{
+	//		obj->GetMeshRenderer()->SetMaterial(RESOURCES->Get<Material>(L"Veigar"));
+	//	}
+	//	CUR_SCENE->Add(obj);
+	//}
+
 	// Mesh
 	{
 		auto obj = make_shared<GameObject>();
 		obj->GetOrAddTransform()->SetLocalPosition(Vec3(0.f));
+		obj->AddComponent(make_shared<MeshRenderer>());
+		{
+			obj->GetMeshRenderer()->SetMaterial(RESOURCES->Get<Material>(L"Veigar"));
+		}
+		/*{
+			auto mesh = RESOURCES->Get<Mesh>(L"Sphere");
+			obj->GetMeshRenderer()->SetMesh(mesh);
+			obj->GetMeshRenderer()->SetPass(0);
+		}*/
+		/*{
+			auto collider = make_shared<SphereCollider>();
+			collider->SetRadius(0.5f);
+			obj->AddComponent(collider);
+		}*/
+		{
+			auto mesh = RESOURCES->Get<Mesh>(L"Cube");
+			obj->GetMeshRenderer()->SetMesh(mesh);
+			obj->GetMeshRenderer()->SetPass(0);
+		}
+		{
+			auto collider = make_shared<AABBBoxCollider>();
+			collider->GetBoundingBox().Extents = Vec3(0.5f);
+			obj->AddComponent(collider);
+		}
+		/*{
+			obj->GetOrAddTransform()->SetRotation(Vec3(0, 45, 0));
+
+			auto collider = make_shared<OBBBoxCollider>();
+			collider->GetBoundingBox().Extents = Vec3(0.5f);
+			collider->GetBoundingBox().Orientation = Quaternion::CreateFromYawPitchRoll(45, 0, 0);
+			obj->AddComponent(collider);
+		}*/
+
+		CUR_SCENE->Add(obj);
+	}
+
+	{
+		auto obj = make_shared<GameObject>();
+		obj->GetOrAddTransform()->SetLocalPosition(Vec3(3.f, 0.f, 0.f));
 		obj->AddComponent(make_shared<MeshRenderer>());
 		{
 			obj->GetMeshRenderer()->SetMaterial(RESOURCES->Get<Material>(L"Veigar"));
@@ -70,38 +146,8 @@ void CollisionDemo::Init()
 			collider->SetRadius(0.5f);
 			obj->AddComponent(collider);
 		}
-
-		CUR_SCENE->Add(obj);
-	}
-
-	// Mesh
-	{
-		auto obj = make_shared<GameObject>();
-		obj->GetOrAddTransform()->SetLocalPosition(Vec3(3.f, 0.f, 0.f));
-		obj->AddComponent(make_shared<MeshRenderer>());
 		{
-			obj->GetMeshRenderer()->SetMaterial(RESOURCES->Get<Material>(L"Veigar"));
-		}
-		{
-			auto mesh = RESOURCES->Get<Mesh>(L"Cube");
-			obj->GetMeshRenderer()->SetMesh(mesh);
-			obj->GetMeshRenderer()->SetPass(0);
-		}
-		{
-			//auto collider = make_shared<AABBBoxCollider>();
-			//collider->GetBoundingBox().Extents = Vec3(0.5f);
-			//obj->AddComponent(collider);
-		}
-		{
-			obj->GetOrAddTransform()->SetRotation(Vec3(0, 45, 0));
-
-			auto collider = make_shared<OBBBoxCollider>();
-			collider->GetBoundingBox().Extents = Vec3(0.5f);
-			collider->GetBoundingBox().Orientation = Quaternion::CreateFromYawPitchRoll(45, 0, 0);
-			obj->AddComponent(collider);
-
-			auto move = make_shared<MoveScript>();
-			obj->AddComponent(move);
+			obj->AddComponent(make_shared<MoveScript>());
 		}
 
 		CUR_SCENE->Add(obj);
@@ -132,6 +178,6 @@ void CollisionDemo::Render()
 void MoveScript::Update()
 {
 	auto pos = GetTransform()->GetPosition();
-	pos.x -= DT * 1.f;
+	pos.x -= DT * 1.0f;
 	GetTransform()->SetPosition(pos);
 }
